e114ad058095ea49c1cf3875bf7d46654e3c0aab
Martin Dressler <m.dressler321@gmail.com>
2024-11-10 23:38:21 +0100

    Delete DRE0065_PROJECT directory

--- DRE0065_PROJECT/DRE0065/Scale.cpp
+++
@@ -1,4 +0,0 @@
-#include "Scale.h"
-
-Scale::Scale(const vec3& scale) : scale(scale) {}
-void Scale::apply(mat4& matrix) const {matrix = glm::scale(matrix, scale);}
--- DRE0065_PROJECT/DRE0065/constant_vertex_shader.glsl
+++
@@ -1,8 +0,0 @@
-#version 400
-layout (location = 0) in vec3 in_Position;
-
-uniform mat4 modelMatrix;
-uniform mat4 viewMatrix;
-uniform mat4 projectionMatrix;
-
-void main(void) {gl_Position = projectionMatrix * viewMatrix * modelMatrix * vec4(in_Position, 1.0);}
