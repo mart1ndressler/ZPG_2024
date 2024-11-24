ba20861b94381a51824b4d3e54a7bb4b4e90414c
Martin Dressler <m.dressler321@gmail.com>
2024-11-25 00:12:59 +0100

    Delete DRE0065 directory

--- DRE0065/Translation.cpp
+++
@@ -1,4 +0,0 @@
-#include "Translation.h"
-
-Translation::Translation(const vec3& translation) : translation(translation) {}
-void Translation::apply(mat4& matrix) const {matrix = glm::translate(matrix, translation);}
