924385d47ccb14da2197215820c8beb538f4a3cf
Martin Dressler <m.dressler321@gmail.com>
2024-12-03 15:21:51 +0100

    Delete DRE0065_PROJECT/DRE0065 directory

--- DRE0065_PROJECT/DRE0065/fragment_shader.glsl
+++
@@ -1,6 +0,0 @@
-#version 330 core
-out vec4 FragColor;
-
-uniform vec3 objectColor = vec3(0.0f, 1.0f, 0.0f);
-
-void main() {FragColor = vec4(objectColor, 1.0f);}
--- DRE0065_PROJECT/DRE0065/texture_fragment.glsl
+++
@@ -1,8 +0,0 @@
-#version 330 core
-
-in vec2 TexCoord;
-out vec4 FragColor;
-
-uniform sampler2D grassTexture;
-
-void main() {FragColor = texture(grassTexture, TexCoord);}
